6e341086de98104b7ee39e3bff435a625c03f808
Calvin <calvinsoares17@gmail.com>
2024-06-29 21:18:45 -0300

    feat: register route created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { DefaultHomeComponent } from './pages/default-home/default-home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-  { path: 'home', component: DefaultHomeComponent },
-  { path: '', component: LoginComponent },
+  {
+    path: 'home',
+    component: DefaultHomeComponent,
+  },
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
 ];
