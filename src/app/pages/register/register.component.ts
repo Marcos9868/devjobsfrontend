1a8a3d4273f611c8ffaa3bc1cc5e5fca7b27a08a
Calvin <calvinsoares17@gmail.com>
2024-06-30 20:15:40 -0300

    feat: post register user and forms

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +17,33 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  RegisterForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  registerForm: FormGroup;
+
+  router = inject(Router);
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Submitting:', this.registerForm.value);
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (response) => {
+          this.router.navigate(['/']);
+          console.log(response);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+        },
+      });
+    }
+  }
 }
