51b72664d7b8a54e8d247f9e8934289f551230c3
Calvin <calvinsoares17@gmail.com>
2024-06-30 21:53:41 -0300

    feat: onSubmit with login and navigate

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +17,32 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  profileForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-  });
+  profileForm: FormGroup;
+
+  router = inject(Router);
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.profileForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      console.log('Submitting:', this.profileForm.value);
+      this.authService.login(this.profileForm.value).subscribe({
+        next: (response) => {
+          this.router.navigate(['/home']);
+          console.log(response);
+        },
+        error: (error) => {
+          console.error('Login failed', error);
+        },
+      });
+    }
+  }
 }
