5c8b283ebb40ea2505c192009c1c860031f11210
Calvin <calvinsoares17@gmail.com>
2024-06-30 21:47:02 -0300

    feat: auth routes with guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,27 @@ import { Routes } from '@angular/router';
 import { DefaultHomeComponent } from './pages/default-home/default-home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { authorizedGuard } from './core/guards/authorized.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: DefaultHomeComponent,
+    canActivate: [authorizedGuard],
   },
   {
     path: '',
     component: LoginComponent,
+    canActivate: [guestGuard],
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [guestGuard],
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
